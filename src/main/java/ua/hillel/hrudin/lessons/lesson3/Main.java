7a316f49da2e957c7884114de71718f918c2185f
Hrudin Viktor <panterpanv@gmail.com>
2023-04-12 21:51:10 +0300

    add delete

--- src/main/java/ua/hillel/hrudin/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/hrudin/lessons/lesson3/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(2+2);
         System.out.println(3+3);
         System.out.println(4+4);
+        System.out.println(4/2);
     }
 }
