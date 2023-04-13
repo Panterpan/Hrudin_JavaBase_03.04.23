bae0e04aea210ffa544dcd0d6f1e7abbf37d831d
Hrudin Viktor <panterpanv@gmail.com>
2023-04-13 21:09:41 +0300

    Main

---
+++ src/main/java/ua/hillel/hrudin/lessons/lesson4/Main.java
@@ -0,0 +1,9 @@
+package ua.hillel.hrudin.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+       int a = 700;
+       int b = 800;
+       int a = 700;
+    }
+}
