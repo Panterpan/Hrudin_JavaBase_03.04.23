9e770d78deec0f22352d21061da1903ee587ae2c
Hrudin Viktor <panterpanv@gmail.com>
2023-05-18 19:25:14 +0300

    Homework14(пробне дз)

--- src/main/java/ua/hillel/hrudin/lessons/lesson12/Main.java
+++ src/main/java/ua/hillel/hrudin/lessons/lesson12/Main.java
@@ -2,7 +2,6 @@ package ua.hillel.hrudin.lessons.lesson12;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
